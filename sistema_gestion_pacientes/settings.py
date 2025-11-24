0d0be76ba8a73341ab6e0a551cb9aa552d37c578
AlevSmol <avillasmil24@cftsa.cl>
2025-11-24 16:20:53 -0300

    mover datos a carpeta raiz

--- sistema_gestion_pacientes/settings.py
+++ sistema_gestion_pacientes/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,6 +85,9 @@ DATABASES = {
         'PASSWORD': '12345',
         'HOST': 'localhost',
         'PORT': '5432',
+        'OPTIONS': {
+            'client_encoding': 'UTF8',
+        }
     }
 }
 
